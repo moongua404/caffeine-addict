d5c07284505cfca3b222ebd501848a22539e227e
moongua404 <hogun.soongit@gmail.com>
2025-03-29 19:35:19 +0900

    feat: 모드 파일 구현

---
+++ src/main/java/com/caffeineaddict/caffeineaddictmode/CaffeineAddictMode.java
@@ -0,0 +1,37 @@
+package com.caffeineaddict.caffeineaddictmode;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(CaffeineAddictMode.MOD_ID)
+public class CaffeineAddictMode {
+    public static final String MOD_ID = "caffeineaddictmode";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public CaffeineAddictMode() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+    @EventBusSubscriber(modid = MOD_ID, bus = Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(final FMLClientSetupEvent event) {
+            
+        }
+    }
+}
